3c782fdebcde0805d9c3f5182ee042e8b9cab097
Julien ARAUJO DA SILVA <juaraujoda>
2025-02-12 17:06:01 +0100

    ajout

---
+++ WebApplicationJuaraujoda/Dto/PlayerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Dto
+{
+    public class PlayerDto
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public float Height { get; set; }
+        public string? Nationality { get; set; }
+        public HandPlay HandPlay { get; set; }
+
+    }
+}
---
+++ WebApplicationJuaraujoda/WebApplicationJuaraujoda/Controllers/PlayersController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Stub;
+using Dto;
+using System.Collections.Generic;
+
+namespace WebApplicationJuaraujoda.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayersController : ControllerBase, IPlayerService
+    {
+        private List<PlayerDto> _players = Stub.StubTennis.GetPlayers();
+
+        public IEnumerable<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = _players;
+                if (result == null)
+                {
+                    return (IEnumerable<IActionResult>)NotFound();
+                }
+                else
+                {
+                    return (IEnumerable<IActionResult>)Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (IEnumerable<IActionResult>)StatusCode(500);
+            }
+
+        }
+
+        public IEnumerable<IActionResult> Getplayers(int index, int count)
+        {
+            try
+            {
+                var result = _players.FirstOrDefault();
+                if (result == null)
+                {
+                    return (IEnumerable<IActionResult>)NotFound();
+                }
+                else
+                {
+                    return (IEnumerable<IActionResult>)Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (IEnumerable<IActionResult>)StatusCode(500);
+            }
+        }
+    }
+}
